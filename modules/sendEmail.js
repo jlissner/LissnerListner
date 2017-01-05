6e446c172fff8f218f26d6f09e916380bbbfa49c
jlissner <jlissner@gmail.com>
2017-01-04 20:13:36 -0600

    Update send email to show how to use new formatting

--- modules/sendEmail.js
+++ modules/sendEmail.js
@@ -4,8 +4,8 @@ const smtp = require('../config/smtp');
 const sendEmail = function(toEmail, subject, template, rawHTML, fromEmail) {
 	const htmlstream = template ? fs.createReadStream('./emailTemplates/' + template + '.html') : rawHTML;
 	const mailOptions = {
-		from: fromEmail || 'marketing@stellaroute.com', // sender address
-		to: toEmail, // list of receivers
+		from: fromEmail || '"Stellaroute" <marketing@stellaroute.com>', // sender address (formatted to be named)
+		to: toEmail, // list of receivers (can be array)
 		subject: subject, // Subject line
 		//text: 'Hello world', // plaintext body
 		html: htmlstream // html body
