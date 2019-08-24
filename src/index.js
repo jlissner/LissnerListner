adcb4193c1ab7db3529a4b5a253178461412c85f
jlissner <jlissner@gmail.com>
2019-08-24 08:35:43 -0500

    dont register the service worker ever

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-import registerServiceWorker, { unregister } from './registerServiceWorker';
+import
+	// registerServiceWorker,
+	{ unregister }
+from './registerServiceWorker';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-registerServiceWorker();
+// registerServiceWorker();
 unregister();
