40933d7a66578dae9a8f524d7a75d8cd64ac3316
jlissner <jlissner@gmail.com>
2019-08-24 08:34:20 -0500

    unregister service to try and fix breaking cache issues

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker, { unregister } from './registerServiceWorker';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
+unregister();
